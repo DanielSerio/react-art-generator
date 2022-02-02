49edd2dbbd44f6ebddb64f3b3da5e29643b22fc5
DanielSerio <dserio.d@gmail.com>
2022-02-02 15:28:02 -0500

    added scss to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './components/App'
 import { AppProvider } from './context'
+import './scss/style.scss'
 
 const root: HTMLElement = (document.getElementById('root') || document.body)
 
