c56ca3e6ad94e84a4d2e989b93adb10ca4be1aef
DanielSerio <dserio.d@gmail.com>
2022-02-02 15:56:03 -0500

    added font reset styles

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
       {
         test: /\.ts(x?)$/,
         use: 'ts-loader',
-        exclude: /node_modules/
+        exclude: /node_modules|\S+\.test\.ts/
       },
       {
         test: /\.scss$/,
