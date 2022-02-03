f9204a68db95b7008c8fbe9be751dd7bad9bb830
DanielSerio <dserio.d@gmail.com>
2022-02-03 12:35:26 -0500

    added min shape count to form

--- src/components/forms/MainForm.tsx
+++ src/components/forms/MainForm.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import { FieldError, useForm } from 'react-hook-form'
+import { Controller, FieldError, useForm, ControllerRenderProps } from 'react-hook-form'
 import FormControl, { FormControlProps } from './FormControl'
+import RCSlider, { createSliderWithTooltip } from 'rc-slider'
 
 export interface MainFormParams {
   height: number
   width: number
+  count: number
 }
 
 export type RequiredFieldParams = [keyof MainFormParams, { required: string }]
 
 export default function MainForm () {
-  const { register, handleSubmit, formState: { errors } } = useForm<MainFormParams>({
+  const { register, handleSubmit, control, formState: { errors } } = useForm<MainFormParams>({
     mode: 'onBlur',
     defaultValues: {
       height: 500,
-      width: 350
+      width: 350,
+      count: 250
     }
   })
+  const Slider = createSliderWithTooltip(RCSlider)
+  const Range = createSliderWithTooltip(RCSlider.Range)
 
   const heightParams: RequiredFieldParams = ['height', { required: 'Height is required.' }]
   const widthParams: RequiredFieldParams = ['width', { required: 'Width is required.' }]
@@ -37,6 +42,21 @@ export default function MainForm () {
         </FormControl>
         <FormControl labelText='Width' inline error={message('width')}>
           <input type="number" className="input" {...register(...widthParams)} />
+        </FormControl>
+      </fieldset>
+      <fieldset>
+        <legend>Properties</legend>
+        <FormControl labelText='Shape Count'>
+          <Controller name="count" control={control} render={
+            ({ field: { onChange, value, ref } }: { field: ControllerRenderProps<MainFormParams, 'count'>}) => {
+              return (
+                <div className="slider-area">
+                  <Slider ref={ref} min={64} max={600} value={value} onChange={onChange} />
+                  <span className="slider-display">{value}</span>
+                </div>
+              )
+            }
+          }/>
         </FormControl>
       </fieldset>
     </form>
